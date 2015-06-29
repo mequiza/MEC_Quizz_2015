5e2d868cf0955053c8f28146df5852b7fee9e4e6
mequiza <mequiza3@gmail.com>
2015-06-29 19:32:58 +0200

    Inicializa BD

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,7 +10,7 @@ var models = require('../models/models.js');
  // GET /quizes/answer
  exports.answer = function(req, res){
    models.Quiz.findAll().success(function(quiz){
-     if (req.query.respuesta === quiz[0].repuesta){
+     if (req.query.respuesta === quiz[0].respuesta){
      res.render('quizes/answer', {respuesta: 'Correcto'});
    } else {
      res.render('quizes/answer', {respuesta: 'Incorrecto'});
