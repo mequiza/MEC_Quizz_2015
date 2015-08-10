8f4c6aa64b326ba94efbefdb7f39aa5d9bab72d8
mequiza <mequiza3@gmail.com>
2015-08-10 09:03:06 +0200

    media

--- models/quiz.js
+++ models/quiz.js
@@ -10,7 +10,7 @@ module.exports = function(sequelize, DataTypes) {
 			validate:{notEmpty: {msg:"¡Falta Respuesta!"}}
 		},
 		image: {
-			type:DataTypes.STRING
+			type:DataTypes.STRING.BINARY
 		}
 	});
 }
